26b60f03a04cb474f0d7262c7c7339199a1fe6bf
Jiunian <jiunian090@gmail.com>
2026-04-12 16:10:04 +0800

    feat: 升级版本至3.6.0并更新更新日志
    新增个人中心界面和版本同步机制，优化系统性能和界面布局，修复已知问题

--- utils/versionInfo.js
+++ utils/versionInfo.js
@@ -1,7 +1,7 @@
 // 自动生成的版本信息文件
 const versionInfo = {
-  "version": "3.5.4",
-  "buildTime": "2026-04-12T07:57:07.628Z",
+  "version": "3.6.0",
+  "buildTime": "2026-04-12T12:00:00.000Z",
   "commit": "unknown"
 };
 module.exports = versionInfo;
