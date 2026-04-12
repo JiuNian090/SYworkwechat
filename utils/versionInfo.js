6af87b62f2caa4badf122a708b9860252af5298b
Jiunian <jiunian090@gmail.com>
2026-04-12 15:59:05 +0800

    chore: 更新构建时间戳

--- utils/versionInfo.js
+++ utils/versionInfo.js
@@ -1,7 +1,7 @@
 // 自动生成的版本信息文件
 const versionInfo = {
   "version": "3.5.4",
-  "buildTime": "2026-04-12T07:46:21.375Z",
+  "buildTime": "2026-04-12T07:57:07.628Z",
   "commit": "unknown"
 };
 module.exports = versionInfo;
