f9a29bc1f060fe043419fe62c2d17bcddd8c6f03
Jiunian <jiunian090@gmail.com>
2026-04-16 21:07:45 +0800

    chore: 更新版本号至3.7.1并添加更新日志
    - 新增多项备份系统优化和功能增强
    - 修复多个图片备份恢复相关的问题
    - 优化用户体验和操作提示信息

--- utils/versionInfo.js
+++ utils/versionInfo.js
@@ -1,7 +1,7 @@
 // 自动生成的版本信息文件
 const versionInfo = {
-  "version": "3.6.2",
-  "buildTime": "2026-04-16T13:01:28.184Z",
+  "version": "3.7.1",
+  "buildTime": "2026-04-16T13:30:00.000Z",
   "commit": "unknown"
 };
 module.exports = versionInfo;
