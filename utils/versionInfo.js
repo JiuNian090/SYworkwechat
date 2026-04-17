9929a10b5fea0247c10905cec4fc09880bc4e116
Jiunian <jiunian090@gmail.com>
2026-04-17 21:15:59 +0800

    feat: 升级版本至3.7.4并新增排班自定义标签功能
    新增排班自定义标签功能，用户可在添加排班时输入标签名
    更新项目版本号及相关配置文件
    修改更新日志规范说明

--- utils/versionInfo.js
+++ utils/versionInfo.js
@@ -1,7 +1,7 @@
 // 自动生成的版本信息文件
 const versionInfo = {
-  "version": "3.7.1",
-  "buildTime": "2026-04-16T13:27:38.357Z",
+  "version": "3.7.4",
+  "buildTime": "2026-04-17T13:15:10.157Z",
   "commit": "unknown"
 };
 module.exports = versionInfo;
