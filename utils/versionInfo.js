daa7f5b6e7e27cdd1c42dbc3a9090a06123438c4
Jiunian <jiunian090@gmail.com>
2026-04-19 16:51:37 +0800

    chore: 更新版本号至3.7.8并同步相关文件
    更新项目版本号至3.7.8，同步修改project.config.json、package.json和versionInfo.js文件
    更新CHANGELOG.md添加v3.7.8版本变更内容
    调整项目规则文档中的更新日志规范说明

--- utils/versionInfo.js
+++ utils/versionInfo.js
@@ -1,7 +1,7 @@
 // 自动生成的版本信息文件
 const versionInfo = {
-  "version": "3.7.4",
-  "buildTime": "2026-04-17T13:15:10.157Z",
+  "version": "3.7.8",
+  "buildTime": "2026-04-19T08:50:37.438Z",
   "commit": "unknown"
 };
 module.exports = versionInfo;
