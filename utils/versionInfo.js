c50fa000f3ec5af28800b6a78bde48018ad687f7
Jiunian <jiunian090@gmail.com>
2026-04-25 00:42:12 +0800

    chore: 更新版本号至3.8.0并添加更新日志
    - 更新package.json、project.config.json和versionInfo.js中的版本号
    - 在CHANGELOG.md中添加v3.8.0的更新内容

--- utils/versionInfo.js
+++ utils/versionInfo.js
@@ -1,7 +1,7 @@
 // 自动生成的版本信息文件
 const versionInfo = {
-  "version": "3.7.9",
-  "buildTime": "2026-04-19T08:50:37.438Z",
+  "version": "3.8.0",
+  "buildTime": "2026-04-24T16:41:48.550Z",
   "commit": "unknown"
 };
 module.exports = versionInfo;
