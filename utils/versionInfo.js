6782e7af140a510555a2a44846756ae7ece0cdf9
Jiunian <jiunian090@gmail.com>
2026-04-25 13:15:42 +0800

    chore: 更新版本号至3.8.1并更新变更日志
    更新项目版本号及相关文件中的版本信息
    更新CHANGELOG.md记录3.8.1版本的优化和修复内容

--- utils/versionInfo.js
+++ utils/versionInfo.js
@@ -1,7 +1,7 @@
 // 自动生成的版本信息文件
 const versionInfo = {
-  "version": "3.8.0",
-  "buildTime": "2026-04-24T16:41:48.550Z",
+  "version": "3.8.1",
+  "buildTime": "2026-04-25T05:15:16.108Z",
   "commit": "unknown"
 };
 module.exports = versionInfo;
