74acfca22c74f2f4f1402c74ea990c2c9f5031a5
Jiunian <jiunian090@gmail.com>
2026-04-26 02:36:07 +0800

    chore: 更新版本号至3.8.4并更新变更日志
    更新package.json、project.config.json和versionInfo.js中的版本号
    添加v3.8.4的变更日志，记录UI优化相关改动

--- utils/versionInfo.js
+++ utils/versionInfo.js
@@ -1,7 +1,7 @@
 // 自动生成的版本信息文件
 const versionInfo = {
-  "version": "3.8.3",
-  "buildTime": "2026-04-25T18:17:04.867Z",
+  "version": "3.8.4",
+  "buildTime": "2026-04-25T18:35:43.031Z",
   "commit": "unknown"
 };
 module.exports = versionInfo;
