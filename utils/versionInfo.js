b4001207456ca6f1d92702aa0bb1447842740f32
Jiunian <jiunian090@gmail.com>
2026-04-26 19:58:15 +0800

    chore: 更新项目版本至3.8.7并更新变更日志
    更新项目配置文件、package.json和版本信息文件中的版本号至3.8.7
    同步更新CHANGELOG.md记录3.8.7版本变更内容

--- utils/versionInfo.js
+++ utils/versionInfo.js
@@ -1,8 +1,8 @@
 'use strict';
 // 自动生成的版本信息文件
 const versionInfo = {
-  'version': '3.8.4',
-  'buildTime': '2026-04-26T08:50:01.540Z',
-  'commit': 'unknown'
+  "version": "3.8.7",
+  "buildTime": "2026-04-26T11:57:30.455Z",
+  "commit": "unknown"
 };
 module.exports = versionInfo;
