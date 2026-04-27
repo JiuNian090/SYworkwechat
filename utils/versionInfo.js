797f530b51f9dd9b35dd813646dc9088844b5f61
Jiunian <jiunian090@gmail.com>
2026-04-28 01:47:34 +0800

    feat: 升级版本至3.9.0并更新更新日志
    新增声明与政策模块、今日心语卡片功能，增强每日消息功能，优化多个界面样式

--- utils/versionInfo.js
+++ utils/versionInfo.js
@@ -1,8 +1,8 @@
 'use strict';
 // 自动生成的版本信息文件
 const versionInfo = {
-  "version": "3.8.8",
-  "buildTime": "2026-04-27T11:00:30.298Z",
+  "version": "3.9.0",
+  "buildTime": "2026-04-27T17:47:03.103Z",
   "commit": "unknown"
 };
 module.exports = versionInfo;
