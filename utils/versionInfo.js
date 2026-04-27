76f1da4fadcc82617c310683d7626cc9fc652eef
Jiunian <jiunian090@gmail.com>
2026-04-27 19:01:03 +0800

    chore: 更新版本号至3.8.8并同步更新日志
    更新package.json、project.config.json和versionInfo.js中的版本号至3.8.8
    同步更新CHANGELOG.md和README.md中的更新日志内容

--- utils/versionInfo.js
+++ utils/versionInfo.js
@@ -1,8 +1,8 @@
 'use strict';
 // 自动生成的版本信息文件
 const versionInfo = {
-  "version": "3.8.7",
-  "buildTime": "2026-04-26T11:57:30.455Z",
+  "version": "3.8.8",
+  "buildTime": "2026-04-27T11:00:30.298Z",
   "commit": "unknown"
 };
 module.exports = versionInfo;
