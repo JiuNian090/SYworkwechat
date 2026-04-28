cee981d1c7bede4f6affb15ddc8100a2a8e4c0f8
Jiunian <jiunian090@gmail.com>
2026-04-28 21:23:51 +0800

    docs: 更新版本信息和更新日志
    更新版本信息中的构建时间，并同步更新README和CHANGELOG中的版本更新内容

--- utils/versionInfo.js
+++ utils/versionInfo.js
@@ -2,7 +2,7 @@
 // 自动生成的版本信息文件
 const versionInfo = {
   "version": "3.10.0",
-  "buildTime": "2026-04-28T05:28:26.478Z",
+  "buildTime": "2026-04-28T13:22:39.270Z",
   "commit": "unknown"
 };
 module.exports = versionInfo;
