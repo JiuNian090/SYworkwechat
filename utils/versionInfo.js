2b50e64caab4d2a0dda1dc2b3794edef09760edf
Jiunian <jiunian090@gmail.com>
2026-04-28 22:16:26 +0800

    chore: 更新版本号至3.10.1并同步相关文件
    更新package.json、project.config.json、versionInfo.js中的版本号
    更新README.md和CHANGELOG.md中的更新日志

--- utils/versionInfo.js
+++ utils/versionInfo.js
@@ -1,8 +1,8 @@
 'use strict';
 // 自动生成的版本信息文件
 const versionInfo = {
-  "version": "3.10.0",
-  "buildTime": "2026-04-28T13:22:39.270Z",
+  "version": "3.10.1",
+  "buildTime": "2026-04-28T14:15:51.982Z",
   "commit": "unknown"
 };
 module.exports = versionInfo;
