845d1a7047ddd767c91f4898bcab39dd9f3019b3
Jiunian <jiunian090@gmail.com>
2026-05-02 23:07:03 +0800

    fix: 移除未使用的api.js文件并更新构建时间
    删除已弃用的api.js文件，该文件包含旧的云函数调用和HTTP请求逻辑
    更新版本信息文件中的构建时间戳

--- utils/versionInfo.js
+++ utils/versionInfo.js
@@ -2,7 +2,7 @@
 // 自动生成的版本信息文件
 const versionInfo = {
   "version": "3.10.1",
-  "buildTime": "2026-04-28T14:15:51.982Z",
+  "buildTime": "2026-05-02T15:06:10.686Z",
   "commit": "unknown"
 };
 module.exports = versionInfo;
