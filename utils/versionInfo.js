51c14b76874dc3fae22321d0e5d10ed7ef4f5d09
Jiunian <jiunian090@gmail.com>
2026-05-06 02:26:12 +0800

    chore: 更新版本号至4.0.1并同步更新日志
    更新package.json、project.config.json和versionInfo.js中的版本号至4.0.1
    同步更新README.md、CHANGELOG.md和changelog.ts中的更新日志内容

--- utils/versionInfo.js
+++ utils/versionInfo.js
@@ -1,8 +1,8 @@
 'use strict';
 // 自动生成的版本信息文件
 const versionInfo = {
-  'version': '4.0.0',
-  'buildTime': '2026-05-02T20:18:26.913Z',
+  'version': '4.0.1',
+  'buildTime': '2026-05-05T18:25:50.731Z',
   'commit': 'unknown'
 };
 module.exports = versionInfo;
